37669872bbd3509086b7b79a93d694448fa3a14f
Marcelo Luis <marcelo.9889@hotmail.com>
2021-03-16 13:10:03 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -8,8 +8,18 @@
     //echo $root;
     
     //Carrega uma lista de usuários
-    $lista = Usuario::getList(); 
+    //$lista = Usuario::getList(); 
 
-    echo json_encode($lista);
+    //echo json_encode($lista);
+
+    //carrega uma lista de usuários buscando pelo login
+    //$search = Usuario::search("jo");
+    //echo json_encode($search);
+
+    //carrega um usuário usando o login e a senha
+    $usuario = new Usuario();
+    $usuario->login("root", "!@#$");
+    
+    echo $usuario;
 
 ?>
